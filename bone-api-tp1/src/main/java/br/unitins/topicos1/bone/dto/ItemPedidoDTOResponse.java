528a31990081e82a50df31c0b8e9699d684fdc34
MatheusRN123 <mrnatividade123@gmail.com>
2025-12-05 07:30:39 -0300

    F

--- bone-api-tp1/src/main/java/br/unitins/topicos1/bone/dto/ItemPedidoDTOResponse.java
+++ bone-api-tp1/src/main/java/br/unitins/topicos1/bone/dto/ItemPedidoDTOResponse.java
@@ -11,9 +11,12 @@ public record ItemPedidoDTOResponse(
     Double subtotal
 ) {
     public static ItemPedidoDTOResponse valueOf(ItemPedido itemPedido){
+
+        Long idPedido = itemPedido.getPedido() != null ? itemPedido.getPedido().getId() : null;
+
         return new ItemPedidoDTOResponse(
             itemPedido.getId(),
-            itemPedido.getPedido().getId(),
+            idPedido,
             BoneDTOResponse.valueOf(itemPedido.getBone()),
             itemPedido.getQuantidade(),
             itemPedido.getPreco(),
--- bone-api-tp1/src/main/java/br/unitins/topicos1/bone/model/Pagamento.java
+++ bone-api-tp1/src/main/java/br/unitins/topicos1/bone/model/Pagamento.java
@@ -2,7 +2,6 @@ package br.unitins.topicos1.bone.model;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
